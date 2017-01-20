406aba0f1f4806b036a39f4f8f89474e512ba253
Michael Fotiadis <michaelfotiadis@gmail.com>
2017-01-20 14:49:07 +0000

    Initial Android Studio migration

---
+++ app/src/main/java/com/michaelfotiadis/locationmanagerviewer/containers/MyLocationData.java
@@ -0,0 +1,236 @@
+package com.michaelfotiadis.locationmanagerviewer.containers;
+
+import android.location.GpsSatellite;
+import android.location.Location;
+
+import com.michaelfotiadis.locationmanagerviewer.utils.Logger;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class MyLocationData {
+
+	private final String TAG = "GPS Data Object";
+	// Unit Suffixes
+	private final String _not_available = "N/A";
+	private final String _meters_seconds = " m/sec";
+	private final String _milliseconds = " ms";
+	private final String _degrees = " deg";
+	private final String _metres = " m";
+	private final String _confidence = " (68% confidence)";
+	private Location location;
+	private CircularFifoQueue<String> nmeaBuffer;
+	private List<GpsSatellite> satellites;
+	private int satellitesInFix = 0;
+	private int nmeaBufferSize = 50;
+	private String mGPSEvent = "";
+
+	public MyLocationData() {
+		nmeaBuffer = new CircularFifoQueue<>(nmeaBufferSize);
+	}
+
+	public MyLocationData(Location location) {
+		setLocation(location);
+	}
+
+	public void appendToNmea(String nmea) {
+		if (!nmea.equals("")) {
+			nmeaBuffer.add(nmea);
+		}
+	}
+
+	public float getAccuracy() {
+		if (this.location != null) {
+			return location.getAccuracy();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getAccuracyAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getAccuracy()) + _metres + _confidence;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public double getAltitude() {
+		if (this.location != null) {
+			return location.getAltitude();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getAltitudeAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getAltitude()) +_metres;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public float getBearing() {
+		if (this.location != null) {
+			return location.getBearing();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getBearingAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getBearing()) + _degrees;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public long getCurrentTime() {
+		return Calendar.getInstance().getTimeInMillis();
+	}
+
+
+	public String getCurrentTimeAsString() {
+		if (getUtcFixTime() > 0) {
+			return String.valueOf(getCurrentTime()) + _milliseconds;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public String getGPSEvent() {
+		return mGPSEvent;
+	}
+
+	public void setGPSEvent(String mGPSEvent) {
+		this.mGPSEvent = mGPSEvent;
+	}
+
+	public double getLatitude() {
+		if (this.location != null) {
+			return location.getLatitude();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getLatitudeAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getLatitude()) + _degrees;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		if (location != null) {
+			this.location = location;
+		} else {
+			Logger.e(TAG, "Null Location");
+		}
+	}
+
+	public double getLongitude() {
+		if (this.location != null) {
+			return location.getLongitude();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getLongitudeAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getLongitude() + _degrees);
+		} else {
+			return _not_available;
+		}
+	}
+
+	public int getMaxSatellites() {
+		return getSatellitesInFix();
+	}
+
+	public String getNmea() {
+		return nmeaBuffer.toString();
+	}
+
+	public String getProvider() {
+		if (this.location != null) {
+			return location.getProvider();
+		} else {
+			return _not_available;
+		}
+	}
+
+	public List<GpsSatellite> getSatellites() {
+		return satellites;
+	}
+
+	public void setSatellites(List<GpsSatellite> satellites) {
+		this.satellites = satellites;
+	}
+
+	public int getSatellitesSize() {
+		if (satellites != null ) {
+			return satellites.size();
+		} else {
+			return  0;
+		}
+	}
+
+	public float getSpeed() {
+		if (this.location != null) {
+			return location.getSpeed();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getSpeedAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getSpeed() + _meters_seconds);
+		} else {
+			return _not_available;
+		}
+	}
+
+	public String getTimeSinceLastFixAsString() {
+		if (this.location != null && getUtcFixTime() > 0) {
+			return String.valueOf(getCurrentTime() - getUtcFixTime()) + _milliseconds;
+		} else {
+			return _not_available;
+		}
+	}
+
+	public long getUtcFixTime() {
+		if (this.location != null) {
+			return location.getTime();
+		} else {
+			return 0;
+		}
+	}
+
+	public String getUtcFixTimeAsString() {
+		if (this.location != null) {
+			return String.valueOf(location.getTime() + _milliseconds);
+		} else {
+			return _not_available;
+		}
+	}
+
+	public int getSatellitesInFix() {
+		return satellitesInFix;
+	}
+
+	public void setSatellitesInFix(int satellitesInFix) {
+		this.satellitesInFix = satellitesInFix;
+	}
+}
---
+++ app/src/main/java/com/michaelfotiadis/locationmanagerviewer/utils/MergeAdapterBuilder.java
@@ -0,0 +1,213 @@
+package com.michaelfotiadis.locationmanagerviewer.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.location.GpsSatellite;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.commonsware.cwac.merge.MergeAdapter;
+import com.michaelfotiadis.locationmanagerviewer.R;
+import com.michaelfotiadis.locationmanagerviewer.datastore.Singleton;
+
+import java.util.List;
+
+/**
+ * Class for adding predefined views to a Merge Adapter
+ * @author Michael Fotiadis
+ *
+ */
+public class MergeAdapterBuilder {
+
+	private final String TAG = "Merge Adapter Builder";
+	private final Activity mActivity;
+	private final MergeAdapter mAdapter;
+	
+	public MergeAdapterBuilder (Activity activity) {
+		mActivity = activity;
+		mAdapter = new MergeAdapter();
+	}
+
+	/**
+	 * Append a header to the MergeAdapter
+	 * @param layoutInflater Layout Inflater
+	 * @param adapter Merge Adapter
+	 * @param data String data
+	 */
+	@SuppressLint("InflateParams")
+	public void appendHeader(final String title){
+		
+		final LinearLayout layout = (LinearLayout) mActivity.getLayoutInflater().inflate(R.layout.list_item_view_header, null);
+		if (layout == null) {
+			Logger.e(TAG, "NULL LAYOUT");
+			return;
+		} 
+		final TextView tvTitle = (TextView) layout.findViewById(R.id.title);
+		tvTitle.setText(title);
+		mAdapter.addView(layout);
+	}
+
+	/**
+	 * Append Body Text to the Merge Adapter
+	 * @param layoutInflater Layout Inflater
+	 * @param adapter Merge Adapter
+	 * @param data String data
+	 */
+	@SuppressLint("InflateParams")
+	public void appendSimpleText(final String data){
+		final LinearLayout layout = (LinearLayout) mActivity.getLayoutInflater().inflate(R.layout.list_item_view_textview, null);
+		if (layout == null) {
+			Logger.e(TAG, "NULL LAYOUT");
+			return;
+		} 
+
+		final TextView tvData = (TextView) layout.findViewById(R.id.data);
+		tvData.setText(data);
+		mAdapter.addView(layout);
+	}
+
+	public ListAdapter generateSatelliteAdapter() {
+		
+		appendHeader(mActivity.getString(R.string.header_satellite_list));
+		
+		List<GpsSatellite> satellites = Singleton.getInstance().getGPSData().getSatellites();
+		
+		if (satellites == null || satellites.size() == 0) {
+			appendSimpleText(mActivity.getString(R.string.info_satellite_used));
+			return mAdapter;
+		}
+		
+		int i = 0;
+		for (GpsSatellite satellite : satellites) {
+			i++;
+			appendHeader(mActivity.getString(R.string.header_satellite) + i);
+			appendSimpleText(mActivity.getString(R.string.info_satellite_prn)  
+					+ satellite.getPrn());
+			appendSimpleText(mActivity.getString(R.string.info_satellite_used)  
+					+ satellite.usedInFix());
+			appendSimpleText(mActivity.getString(R.string.info_satellite_azimuth)  
+					+ satellite.getAzimuth() + mActivity.getString(R.string.info_degrees));
+			appendSimpleText(mActivity.getString(R.string.info_satellite_elevation)  
+					+ satellite.getElevation() + mActivity.getString(R.string.info_degrees));
+			appendSimpleText(mActivity.getString(R.string.info_satellite_snr)  
+					+ satellite.getSnr());
+			appendSimpleText(mActivity.getString(R.string.info_satellite_almanac)  
+					+ satellite.hasAlmanac());
+			appendSimpleText(mActivity.getString(R.string.info_satellite_ephemeris)  
+					+ satellite.hasEphemeris());
+		}
+		
+		return mAdapter;
+	}
+
+	public ListAdapter generateNMEAAdapter() {
+		appendHeader(mActivity.getString(R.string.header_nmea));
+		appendSimpleText(Singleton.getInstance().getGPSData().getNmea());
+		
+		return mAdapter;
+	}
+	
+	public ListAdapter generateGPSAdapter() {
+
+		appendHeader(mActivity.getString(R.string.header_network_status));
+		appendSimpleText(mActivity.getString(R.string.info_gps_adapter)  
+				+ Singleton.getInstance().getNetworkStatus().isGPSEnabledAsString());
+		appendSimpleText(mActivity.getString(R.string.info_provider)  
+				+ Singleton.getInstance().getGPSData().getProvider());
+
+		appendHeader(mActivity.getString(R.string.header_location));
+		appendSimpleText(mActivity.getString(R.string.info_latitude)  
+				+ Singleton.getInstance().getGPSData().getLatitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_longitude)   
+				+ Singleton.getInstance().getGPSData().getLongitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_altitude)   
+				+ Singleton.getInstance().getGPSData().getAltitudeAsString());
+
+		appendHeader(mActivity.getString(R.string.header_details));
+		appendSimpleText(mActivity.getString(R.string.info_accuracy)  
+				+ Singleton.getInstance().getGPSData().getAccuracyAsString());
+		appendSimpleText(mActivity.getString(R.string.info_bearing)  
+				+ Singleton.getInstance().getGPSData().getBearingAsString());
+		appendSimpleText(mActivity.getString(R.string.info_speed)   
+				+ Singleton.getInstance().getGPSData().getSpeedAsString());
+
+
+		appendHeader(mActivity.getString(R.string.header_other));
+		appendSimpleText(mActivity.getString(R.string.info_utc_time)
+				+ Singleton.getInstance().getGPSData().getUtcFixTimeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_event)  
+				+ Singleton.getInstance().getGPSData().getGPSEvent());
+		appendSimpleText(mActivity.getString(R.string.info_satellites_total)  
+				+ Singleton.getInstance().getGPSData().getSatellitesSize());
+		appendSimpleText(mActivity.getString(R.string.info_satellites_in_fix)  
+				+ Singleton.getInstance().getGPSData().getSatellitesInFix());
+		
+		return mAdapter;
+	}
+
+	public ListAdapter generateNetworkAdapter() {
+
+		appendHeader(mActivity.getString(R.string.header_network_status));
+		appendSimpleText(mActivity.getString(R.string.info_network_adapter)   
+				+ Singleton.getInstance().getNetworkStatus().isCellNetworkEnabledAsString());
+		appendSimpleText(mActivity.getString(R.string.info_provider)  
+				+ Singleton.getInstance().getNetworkData().getProvider());
+
+		appendHeader(mActivity.getString(R.string.header_location));
+		appendSimpleText(mActivity.getString(R.string.info_latitude)  
+				+ Singleton.getInstance().getNetworkData().getLatitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_longitude)  
+				+ Singleton.getInstance().getNetworkData().getLongitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_altitude)  
+				+ Singleton.getInstance().getNetworkData().getAltitudeAsString());
+
+		appendHeader(mActivity.getString(R.string.header_details));
+		appendSimpleText(mActivity.getString(R.string.info_accuracy)  
+				+ Singleton.getInstance().getNetworkData().getAccuracyAsString());
+		appendSimpleText(mActivity.getString(R.string.info_bearing)  
+				+ Singleton.getInstance().getNetworkData().getBearingAsString());
+		appendSimpleText(mActivity.getString(R.string.info_speed)  
+				+ Singleton.getInstance().getNetworkData().getSpeedAsString());
+
+		appendHeader(mActivity.getString(R.string.header_other));
+		appendSimpleText(mActivity.getString(R.string.info_utc_time)  
+				+ Singleton.getInstance().getNetworkData().getUtcFixTimeAsString());
+
+		return mAdapter;
+	}
+
+	public ListAdapter generatePassiveAdapter() {
+
+		appendHeader(mActivity.getString(R.string.header_network_status));
+		appendSimpleText(mActivity.getString(R.string.info_network_adapter)  
+				+ Singleton.getInstance().getNetworkStatus().isCellNetworkEnabledAsString());
+		appendSimpleText(mActivity.getString(R.string.info_gps_adapter)  
+				+ Singleton.getInstance().getNetworkStatus().isGPSEnabledAsString());
+		appendSimpleText(mActivity.getString(R.string.info_provider)  
+				+ Singleton.getInstance().getPassiveData().getProvider());
+
+		appendHeader(mActivity.getString(R.string.header_location));
+		appendSimpleText(mActivity.getString(R.string.info_latitude)  
+				+ Singleton.getInstance().getPassiveData().getLatitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_longitude)  
+				+ Singleton.getInstance().getPassiveData().getLongitudeAsString());
+		appendSimpleText(mActivity.getString(R.string.info_altitude)  
+				+ Singleton.getInstance().getPassiveData().getAltitudeAsString());
+
+		appendHeader(mActivity.getString(R.string.header_details));
+		appendSimpleText(mActivity.getString(R.string.info_accuracy)   
+				+ Singleton.getInstance().getPassiveData().getAccuracyAsString());
+		appendSimpleText(mActivity.getString(R.string.info_bearing)  
+				+ Singleton.getInstance().getPassiveData().getBearingAsString());
+		appendSimpleText(mActivity.getString(R.string.info_speed)  
+				+ Singleton.getInstance().getPassiveData().getSpeedAsString());
+
+		appendHeader(mActivity.getString(R.string.header_other));
+		appendSimpleText(mActivity.getString(R.string.info_utc_time)  
+				+ Singleton.getInstance().getPassiveData().getUtcFixTimeAsString());
+
+		return mAdapter;
+	}
+	
+}
